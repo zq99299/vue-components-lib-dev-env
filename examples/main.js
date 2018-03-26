aa8ff56c21229672e1e8131224de10975403be59
zhuqiang <zhuqiang@tidebuy.net>
2018-03-26 15:00:06 +0800

    增加注释说明

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 开发的时候直接引用组件的入口文件,有热加载效果
 // import VueComponentsLib from '../src/index'
+
+// 开发完成后，切换成打包好的js和css文件，看是否和开发的时候表现一致
 import VueComponentsLib from '../lib/vueComponentsLib.min.js'
 import '../lib/styl/vueComponentsLib.min.css'
 
